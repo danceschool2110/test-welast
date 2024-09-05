36018580e4cded4104d4944a22c82741dce507e1
Nguyen Dan Truong <ntruong1021@gmail.com>
2024-09-05 23:32:30 +0700

    add get repos api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,32 @@
-import express from "express";
+import axios from "axios";
+import express, { Response, Request } from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
+let corsOptions = {
+  origin: ["http://localhost:3000"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello, world!");
+});
+
+app.get("/repos", async (req: Request, res: Response) => {
+  try {
+    const repos = await axios.get(
+      "https://api.github.com/users/freeCodeCamp/repos"
+    );
+    const results = repos.data.filter(
+      (item: any) => !item.fork && item.forks > 5
+    );
+    return res.json({ data: results });
+  } catch (error) {
+    throw new Error("Server error");
+  }
 });
 
 app.listen(port, () => {
